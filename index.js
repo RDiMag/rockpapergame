1a28803ec70634261cff389e725d526f7c851338
Rachel <proofreader_one@yahoo.com>
2021-11-09 20:43:26 -0500

    computerPlay function works

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hi there, planet!");
+const gameOptions = ["Rock", "Paper", "Scissors"];
+
+function computerPlay(gameOptions) {
+    return gameOptions[Math.floor(Math.random() * gameOptions.length)];
+}
+
+console.log(computerPlay);
+
