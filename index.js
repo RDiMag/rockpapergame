310717403c3d5bef1bdbee8de377c375c8c84aed
Rachel <proofreader_one@yahoo.com>
2021-12-11 15:27:25 -0500

    working RPS game and end game at 5 points

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
-// This function creates a random computer selection from the given three options.
 let playerScore = 0;
 let computerScore = 0;
+
+// This function creates a random computer selection from the given three options.
 
 function computerPlay() {
     const gameOptions = ["Rock", "Paper", "Scissors"];
     return gameOptions[Math.floor(Math.random() * gameOptions.length)];
 }
 
-
-document.getElementById('rock').onclick = playRound, computerPlay;
-document.getElementById('paper').onclick = playRound, computerPlay;
-document.getElementById('scissors').onclick = playRound, computerPlay;
+// This connects the button clicks to the computerPlay and playRound functions.
+document.getElementById('rock').onclick = playRound;
+document.getElementById('paper').onclick = playRound;
+document.getElementById('scissors').onclick = playRound;
 document.getElementById('reset').onclick = restartGame;
 
-
+//This is the game function.
 function playRound() {
-    var playerSelection = this.id;
-    var computerSelection = computerPlay();
+    let playerSelection = this.id;
+    let computerSelection = computerPlay();
     console.log("User: " + playerSelection)
     console.log("Computer: " + computerSelection)
 
         if (playerSelection == "rock" && computerSelection == "Scissors"){
             playerScore += 1;
-            winner();
             document.getElementById("results").innerHTML = (`You win! Rock beats scissors. Player: ${playerScore} Computer: ${computerScore}`);
         } else if  (playerSelection == "rock" && computerSelection == "Paper"){
             computerScore += 1;
-            winner();
             document.getElementById("results").innerHTML = (`You lose! Paper beats rock.  Player: ${playerScore} Computer: ${computerScore}`);
         } else if (playerSelection == "scissors" && computerSelection == "Rock"){
             computerScore += 1;
-            winner();
             document.getElementById("results").innerHTML = (`You lose! Rock beats scissors. Player: ${playerScore} Computer: ${computerScore}`);
         } else if (playerSelection == "scissors" && computerSelection == "Paper"){
             playerScore += 1;
-            winner();
             document.getElementById("results").innerHTML = (`You win! Scissors beats paper.  Player: ${playerScore} Computer: ${computerScore}`);
         } else if (playerSelection == "paper" && computerSelection == "Rock"){
             playerScore += 1;
@@ -42,9 +39,16 @@ function playRound() {
         } else if (playerSelection == "paper" && computerSelection == "Scissors"){
             computerScore += 1;
             document.getElementById("results").innerHTML = (`You lose! Scissors beat paper.  Player: ${playerScore} Computer: ${computerScore}`);
-        } else winner();
-            document.getElementById("results").innerHTML = (`You tied!  Player: ${playerScore} Computer: ${computerScore}`);
+        } else {
+            document.getElementById("results").innerHTML = (`You tied!`)
+        }
         
+        winner();
+
+//Winner function should update "gameover" div with a message when playerScore or computerScore reaches 5.
+//It works sometimes but is unpredictable. Sometimes it allows scores far higher than 5.
+//It should also call endGame to disable buttons and update "results" div. 
+
             function winner() {
                 if (playerScore === 5) {
                 endGame()
@@ -58,7 +62,7 @@ function playRound() {
                   document.getElementById("rock").disabled = true;
                   document.getElementById("paper").disabled = true;
                   document.getElementById("scissors").disabled = true;
-                  document.getElementById("results").innerHTML = ("Results:");
+                  document.getElementById("results").innerHTML = (" ");
             }
          
     } 
